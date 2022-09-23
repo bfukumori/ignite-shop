4a9b5a27b06bc5ccaee9190a0a167237f0b35fe1
Bruno Fukumori <brunofukumori@gmail.com>
2022-09-23 00:11:16 -0300

    add carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,50 @@
 import Image from 'next/image';
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
 import { HomeContainer, Product } from '../styles/home';
 import camiseta1 from '../assets/mock/1.png';
 import camiseta2 from '../assets/mock/2.png';
 import camiseta3 from '../assets/mock/3.png';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={camiseta1} width={520} height={480} alt='' />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={camiseta2} width={520} height={480} alt='' />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$79,90</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
         <Image src={camiseta3} width={520} height={480} alt='' />
         <footer>
           <strong>Camiseta X</strong>
