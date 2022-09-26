a69ed9f80a7928673803e16d89e57fb123b775ce
Bruno Fukumori <brunofukumori@gmail.com>
2022-09-26 00:12:03 -0300

    remove api route since using use-shopping-cart-hook

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,6 @@
 import { GetServerSideProps } from 'next';
 import Image from 'next/future/image';
 import Head from 'next/head';
-import Link from 'next/link';
 
 import Stripe from 'stripe';
 import { stripe } from '../lib/stripe';
